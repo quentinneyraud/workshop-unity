653f50451a8b1649bce3f050f6f95fc22fc9d099
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-10-24 16:16:20 +0200

    paralax & position

---
+++ Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject terrain;
+
+	private float minX = 0f;
+	private float maxX = 0f;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		minX = terrain.transform.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		Vector3 newPosition = player.transform.position + offset;
+
+		/*if (newPosition.x < minX) {
+			newPosition.x = minX;
+		} else if (newPosition.x > maxX) {
+			newPosition.x = maxX;
+		}
+*/
+		transform.position = newPosition;
+	}
+}
---
+++ Assets/Paralax.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralax : MonoBehaviour {
+
+	public GameObject camera;
+	public float force;
+
+	private float cameraX;
+
+	// Use this for initialization
+	void Start () {
+		cameraX = camera.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate (){
+		Vector3 tmpPosition = transform.position;
+
+		tmpPosition.x -= (camera.transform.position.x - cameraX) * force;
+
+		transform.position = tmpPosition;
+
+		cameraX = camera.transform.position.x;
+	}
+}
